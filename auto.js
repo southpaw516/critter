0b3f50d1a2dd34ac187dc632dc06038001268384
southpaw516 <southpawobserver@gmail.com>
2017-04-04 12:03:22 -0500

    Added sort_by

--- auto.js
+++ auto.js
@@ -106,6 +106,21 @@ function kidSelect(parent, child, parentButton, workerButton, soldierButton) {
     }
 }
 
+sort_by = function(field, reverse, primer) {
+
+    var key = primer ? function(x) {
+            return primer(x[field]);
+        } : function(x) {
+            return x[field];
+        };
+
+    reverse = !reverse ? 1 : -1;
+
+    return function(a, b) {
+        return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+    };
+};
+
 function autoUpgrade() {
 
     soldierButton = document.getElementsByClassName('upgrade')[8];
